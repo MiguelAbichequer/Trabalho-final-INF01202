e7421f9f29834b3acc7f1be2514a42193801f8ec
MiguelAbichequer <abichequerm@gmail.com>
2025-06-16 23:33:27 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,34 @@
+int main()
+{
 
+    InitWindow(600, 400, "Hello_World");
+    
+    Texture2D texture_player = LoadTexture("path_file.png");
+    
+    Sprite player;
+    Rectangle retangulo;
+    
+    player.texture = texture_player;
+    
+    retangulo.x = 10.0; 
+    retangulo.y = 10.0;
+    retangulo.width = 100;
+    retangulo.height = 100; 
+    
+    player.dest_rect = retangulo; // a hitbox do jogador será o retângulo informado acima.
+    
+
+    while(!WindowShouldClose())
+    {
+        BeginDrawing();
+        
+        ClearBackground(SKYBLUE);
+        
+        DrawTexture(player.texture, {0,0,16,16}, {0.0}, 0.0, RAYWHITE);
+        
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
