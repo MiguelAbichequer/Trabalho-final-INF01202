d53b0af8f5050adaf494529ac23dbca83a2fc133
MiguelAbichequer <abichequerm@gmail.com>
2025-06-19 01:26:28 -0300

    Update game.h

--- include/game/game.h
+++ include/game/game.h
@@ -5,13 +5,17 @@
 #define A 20
 #define L 80 // altura e largura da tela
 
+#define VEL 300 // VELOCIDADE DO CARRO
+
+#define ROT 0.01 // ângulo de rotação gerado pelo movimento do carro
+
 typedef struct{
     Texture2D texture; // textura do sprite.
     Rectangle dest_rect; // hitbox do sprite.
 }Sprite;
 
 
-void move_player(Sprite* player);
+void move_player(Sprite* player, float *angulo);
 void LeMapa(char nome_arq[], char matrix[A][L]);
 
 
