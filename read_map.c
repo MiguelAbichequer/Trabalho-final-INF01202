b9051e5b09cb9e7f41ac8dda9bd10e043eb83176
MiguelAbichequer <abichequerm@gmail.com>
2025-06-19 00:42:02 -0300

    Update read_map.c

--- read_map.c
+++ read_map.c
@@ -1,1 +1,29 @@
+#include "game.h"
+
+void LeMapa(char nome_arq[], char matrix[A][L]);
+{
+    FILE *arq;
+
+    int i;
+    int j;
+
+    arq = fopen(nome_arq, "r");
+    if(arq == NULL)
+    {
+        printf("ERRO");
+    }
+    else
+    {
+
+        for(i = 0; i < A; i++)
+        {
+            for(j = 0; j < L; j++)
+            {
+
+                fgetc(matrix[i][j], 1, arq); // copiar caracteres do mapa para a matriz
+
+            }
+        }
+    }
+}
 
