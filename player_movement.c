e921b1d395cd1639315df0755437bddadd593a93
MiguelAbichequer <abichequerm@gmail.com>
2025-06-19 00:46:46 -0300

    Update player_movement.c

--- player_movement.c
+++ player_movement.c
@@ -1,20 +1,18 @@
-#include "game.h"
-
 void move_player(Sprite* player)
 {
-    if(IsKeyDown(KEY_D)
+    if(IsKeyDown(KEY_D))
     {
-        player->dest_rect.x = player->dest_rect.x + 100.0 * GetFrameTime(); // o parâmetro correspondente ao movimento desejado será atualizado por referência, acessando o campo de um ponteiro player.
+        player->dest_rect.x = player->dest_rect.x + 100.0 * GetFrameTime();
     }
-    else if(IsKeyDown(KEY_A))
+    if(IsKeyDown(KEY_A))
     {
         player->dest_rect.x = player->dest_rect.x - 100.0 * GetFrameTime();
     }
-    else if(IsKeyDown(KEY_S))
+    if(IsKeyDown(KEY_S))
     {
         player->dest_rect.y = player->dest_rect.y - 100.0 * GetFrameTime();
     }
-    else(IsKeyDown(KEY_W))
+    if(IsKeyDown(KEY_W))
     {
         player->dest_rect.y = player->dest_rect.y + 100.0 * GetFrameTime();
     }
