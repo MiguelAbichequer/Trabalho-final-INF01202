c64a0c8b8c04ab75f96d10f31de41c8c89e939a6
MiguelAbichequer <abichequerm@gmail.com>
2025-06-19 01:31:04 -0300

    Update player_movement.c

--- player_movement.c
+++ player_movement.c
@@ -1,19 +1,28 @@
-void move_player(Sprite* player)
+void move_player(Sprite* player, float *angulo)
 {
     if(IsKeyDown(KEY_D))
     {
-        player->dest_rect.x = player->dest_rect.x + 100.0 * GetFrameTime();
+        player->dest_rect.x = player->dest_rect.x + VEL * GetFrameTime();
+
+        *angulo = *angulo + ROT;
     }
     if(IsKeyDown(KEY_A))
     {
-        player->dest_rect.x = player->dest_rect.x - 100.0 * GetFrameTime();
+        player->dest_rect.x = player->dest_rect.x - VEL * GetFrameTime();
+        *angulo = *angulo - ROT;
+
     }
     if(IsKeyDown(KEY_S))
     {
-        player->dest_rect.y = player->dest_rect.y + 100.0 * GetFrameTime();
+        player->dest_rect.y = player->dest_rect.y + VEL * GetFrameTime();
+        *angulo = *angulo + ROT;
+
     }
     if(IsKeyDown(KEY_W))
     {
-        player->dest_rect.y = player->dest_rect.y - 100.0 * GetFrameTime();
+        player->dest_rect.y = player->dest_rect.y - VEL * GetFrameTime();
+
+        *angulo = *angulo + ROT;
+
     }
 }
