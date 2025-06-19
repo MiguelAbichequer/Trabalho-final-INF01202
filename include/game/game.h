bdc70eaafc2426d2da34485f3e5a3adf43e84236
MiguelAbichequer <abichequerm@gmail.com>
2025-06-19 00:41:16 -0300

    Update game.h

--- include/game/game.h
+++ include/game/game.h
@@ -1,6 +1,17 @@
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define A 20
+#define L 80 // altura e largura da tela
+
 typedef struct{
     Texture2D texture; // textura do sprite.
     Rectangle dest_rect; // hitbox do sprite.
 }Sprite;
 
+
 void move_player(Sprite* player);
+void LeMapa(char nome_arq[], char matrix[A][L]);
+
+
