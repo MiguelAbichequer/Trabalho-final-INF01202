abab249a862e96efbd61a750d4b78bbea84407c4
MiguelAbichequer <abichequerm@gmail.com>
2025-06-19 01:27:07 -0300

    Update main.c
    Cubo que gira e anda

--- main.c
+++ main.c
@@ -7,13 +7,15 @@ int main()
 
     InitWindow(600, 400, "Hello_World");
 
-    Image imagem = GenImageColor(40, 30, RED); // gerar provisoriamente uma imagem como textura.
+    Image imagem = GenImageColor(10, 5, RED); // gerar provisoriamente uma imagem como textura.
 
     Texture2D texture_player = LoadTextureFromImage(imagem); // um png será carregado como textura para o jogador.
 
     Sprite player;
 
     Rectangle retangulo;
+
+    float angulo;
 
 
 
@@ -26,15 +28,16 @@ int main()
 
     player.dest_rect = retangulo; // a hitbox do jogador será o retângulo informado acima.
 
-    Rectangle source_rect = {0.0f, 0.0f, (float)player.texture.width, (float)player.texture.height};
-    Vector2 origin = {0.0f, 0.0f};
+    Rectangle source_rect = {10.0f, 10.0f, (float)player.texture.width, (float)player.texture.height}; // passagem das dimensões do retangulo de player para um vetor
+    Vector2 origin = {0.0f, 0.0f}; // vetor que armazena a origem do player, sera atualizado conforme o mapa carregado.
 
+    angulo = 0.0; // o ângulo de rotação começa com 0.0
 
     while(!WindowShouldClose())
     {
         // atualizar
 
-        move_player(&player);
+        move_player(&player, &angulo);
 
 
 
@@ -52,7 +55,7 @@ int main()
         // 0.0f: Ângulo de rotação.
         // RAYWHITE: Cor de tingimento (mantém as cores originais).
 
-        DrawTexturePro(player.texture, source_rect, player.dest_rect, origin, 0.0, RAYWHITE);
+        DrawTexturePro(player.texture, source_rect, player.dest_rect, origin, angulo, RAYWHITE);
 
 
         EndDrawing();
