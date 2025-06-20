a6351f0269eb1a4a9e56cfda54da05e2cd943bd0
MiguelAbichequer <abichequerm@gmail.com>
2025-06-19 22:28:08 -0300

    Update main.c
    Consegui criar um protótipo de menu. A leitura do arquivo .txt para o mapa também funcionou, mas não da maneira correta.

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 int main()
 {
 
-    InitWindow(600, 400, "Hello_World");
+    InitWindow(1200, 800, "Hello_World");
 
     Image imagem = GenImageColor(10, 5, RED); // gerar provisoriamente uma imagem como textura.
 
@@ -15,7 +15,15 @@ int main()
 
     Rectangle retangulo;
 
-    float angulo; // ângulo de rotação do carro. Esse valor muda conforme o movimento dele.
+    float angulo;
+
+    // Variáves para a seleção e construção do mapa:
+
+    int mapa_escolhido = FALSE;
+    int mapa;
+    char nome_arquivo_mapa[100];
+    char matrix[A][L];
+    int i, j, k;
 
 
 
@@ -23,8 +31,8 @@ int main()
 
     retangulo.x = 10.0;
     retangulo.y = 10.0;
-    retangulo.width = 100;
-    retangulo.height = 100;
+    retangulo.width = 30;
+    retangulo.height = 40;
 
     player.dest_rect = retangulo; // a hitbox do jogador será o retângulo informado acima.
 
@@ -33,34 +41,84 @@ int main()
 
     angulo = 0.0; // o ângulo de rotação começa com 0.0
 
-    while(!WindowShouldClose())
+    while (!WindowShouldClose())
     {
-        // atualizar
 
-        move_player(&player, &angulo);
+        // ATUALIZAÇÃO
+
+        if (mapa_escolhido == FALSE) // ESTAMOS NO MENU
+        {
+            // Lógica do menu: verificar se uma tecla foi pressionada
+            if (IsKeyDown(KEY_KP_1) || IsKeyDown(KEY_ONE))
+            {
+
+                mapa_escolhido = TRUE; // Muda para o estado de "jogando"
+
+                strcpy(nome_arquivo_mapa, "mapa1.txt");
+
+            }
+            if (IsKeyDown(KEY_KP_2) || IsKeyDown(KEY_TWO))
+            {
+
+                mapa_escolhido = TRUE;
+
+                strcpy(nome_arquivo_mapa, "mapa2.txt");
+
+            }
+            if (IsKeyDown(KEY_KP_3) || IsKeyDown(KEY_THREE))
+            {
+
+                mapa_escolhido = TRUE;
+
+                strcpy(nome_arquivo_mapa, "mapa3.txt");
+            }
+        }
+        else // ESTAMOS JOGANDO
+        {
+
+            move_player(&player, &angulo);
+
+        }
 
 
-
-        // desenhar
+        // DESENHO
 
         BeginDrawing();
-
         ClearBackground(SKYBLUE);
 
+        if (mapa_escolhido == FALSE) // DESENHANDO O MENU
+        {
+            DrawText("SELECIONE UM MAPA", 20, 20, 40, DARKGREEN);
+            DrawText("1 - Mapa Facil", 20, 70, 30, DARKGREEN);
+            DrawText("2 - Mapa Medio", 20, 110, 30, DARKGREEN);
+            DrawText("3 - Mapa Dificil", 20, 150, 30, DARKGREEN);
+        }
+        else // DESENHANDO O JOGO
+        {
 
-        // player.texture: A textura a ser desenhada.
-        // source_rect: A parte da textura original a ser usada.
-        // player.dest_rect: Onde e com que tamanho desenhar na tela (já atualizado pelo move_player).
-        // origin: Ponto de referência para rotação.
-        // 0.0f: Ângulo de rotação.
-        // RAYWHITE: Cor de tingimento (mantém as cores originais).
+            LeMapa(nome_arquivo_mapa, matrix);
 
-        DrawTexturePro(player.texture, source_rect, player.dest_rect, origin, angulo, RAYWHITE);
+            for(i = 0; i < A; i++)
+            {
+                for(j = 0; j < L; j++)
+                {
+                    if(matrix[i][j] == 'p')
+                    {
+                        DrawTexture(texture_player, j*30,i*40, GREEN);
+                    }
+                }
 
+            }
+
+            DrawTexturePro(player.texture, source_rect, player.dest_rect, origin, angulo, RAYWHITE);
+
+
+        }
 
         EndDrawing();
     }
 
     CloseWindow();
+
     return 0;
 }
