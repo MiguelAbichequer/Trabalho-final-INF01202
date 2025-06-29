86b4c909ef65e092cf7e164b7c051f0ee4e5a162
MiguelAbichequer <abichequerm@gmail.com>
2025-06-29 17:57:58 -0300

    Update draw_map.c

--- functions /draw_map.c
+++ functions /draw_map.c
@@ -1,1 +1,31 @@
+#include "DesenharMapa.h"
+
+void desenhar_mapa(char matriz[][L], Jogador *player)
+{
+    for (int i = 0; i < A; i++)
+    {
+        for (int j = 0; j < L; j++)
+        {
+            int x = j * TAMANHO_BLOCO;
+            int y = i * TAMANHO_BLOCO;
+
+            switch (matriz[i][j])
+            {
+                case 'p':
+                    DrawRectangle(x, y, TAMANHO_BLOCO, TAMANHO_BLOCO, GRAY);
+                    DrawRectangleLines(x, y, TAMANHO_BLOCO, TAMANHO_BLOCO, DARKGRAY);
+                    break;
+                case 'i':
+                    break;
+                case 'j':
+                    break; //outra funcao devera desenhar o jogador na tela
+                case ' ':
+                    DrawRectangle(x, y, TAMANHO_BLOCO, TAMANHO_BLOCO, LIGHTGRAY);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
 // a função deverá desenhar o mapa correspondente.
