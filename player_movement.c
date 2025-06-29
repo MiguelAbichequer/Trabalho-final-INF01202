da65955d9d472db9b625ca9cbe95a1c0cf988035
MiguelAbichequer <abichequerm@gmail.com>
2025-06-29 17:57:15 -0300

    Update player_movement.c

--- player_movement.c
+++ player_movement.c
@@ -1,28 +1,28 @@
-void move_player(Sprite* player, float *angulo)
+#include "MovimentoJogador.h"
+
+void movimentoJogador(Jogador *player)
 {
-    if(IsKeyDown(KEY_D))
+    if (IsKeyDown(KEY_LEFT)) player->rotation -= 5;
+    if (IsKeyDown(KEY_RIGHT)) player->rotation += 5;
+
+    player->speed.x = sin(player->rotation * DEG2RAD) * PLAYER_SPEED;
+    player->speed.y = cos(player->rotation * DEG2RAD) * PLAYER_SPEED;
+
+    if (IsKeyDown(KEY_UP))
     {
-        player->dest_rect.x = player->dest_rect.x + VEL * GetFrameTime();
-
-        *angulo = *angulo + ROT;
+        if (player->acceleration < 1) player->acceleration += 0.02f;
     }
-    if(IsKeyDown(KEY_A))
+    else
     {
-        player->dest_rect.x = player->dest_rect.x - VEL * GetFrameTime();
-        *angulo = *angulo - ROT;
-
+        if (player->acceleration > 0) player->acceleration -= 0.02f;
+        else if (player->acceleration < 0) player->acceleration = 0;
     }
-    if(IsKeyDown(KEY_S))
+    if (IsKeyDown(KEY_DOWN))
     {
-        player->dest_rect.y = player->dest_rect.y + VEL * GetFrameTime();
-        
-
+        if (player->acceleration > 0) player->acceleration -= 0.04f;
+        else if (player->acceleration < 0) player->acceleration -= 0.04f;
     }
-    if(IsKeyDown(KEY_W))
-    {
-        player->dest_rect.y = player->dest_rect.y - VEL * GetFrameTime();
 
-        
-
-    }
+    player->position.x += (player->speed.x * player->acceleration);
+    player->position.y -= (player->speed.y * player->acceleration);
 }
